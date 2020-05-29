f80eb695407f89cfab55881e605213eb79adf730
ntxl1210 <49311469+ntxl1210@users.noreply.github.com>
2020-05-29 21:21:31 +0700

    chỉnh sửa sản phẩm, hóa đơn

--- PythonWeb/sanpham/admin.py
+++ PythonWeb/sanpham/admin.py
@@ -23,55 +23,37 @@ class Sim_Key(resources.ModelResource):
         'Anh',)
 
 class Sim(ImportExportModelAdmin,ExportActionMixin):
-    exclude = ('LoaiGia',)
+    readonly_fields = ('LoaiGia',)
+    list_display = ('SoSim', 'Gia', 'LoaiGia', 'Mang', 'TacVu', 'get_loaisims', 'NgayNhap', 'id',)
     list_per_page = 20
     resource_class = Sim_Key
+
     def save_model(self, request, obj, form, change):
         gia = obj.Gia
-
+        print(gia)
         if gia < 500000:
-            loaigia_obj = SimTheoGia.objects.filter(title="Sim dưới 500 nghìn").last()
-            if loaigia_obj:
-                obj.LoaiGia = loaigia_obj
-            else:
-                loaigia_obj = SimTheoGia.objects.filter(title="Giá khác").last()
-                obj.LoaiGia = loaigia_obj
-        elif gia > 500000 & gia < 1000000:
-            loaigia_obj = SimTheoGia.objects.filter(title="Sim giá 500 - 1 triệu").last()
-            if loaigia_obj:
-                obj.LoaiGia = loaigia_obj
-            else:
-                loaigia_obj = SimTheoGia.objects.filter(title="Giá khác").last()
-                obj.LoaiGia = loaigia_obj
-        elif gia > 1000000 & gia < 3000000:
-            loaigia_obj = SimTheoGia.objects.filter(title="Sim giá 1 triệu - 3 triệu").last()
-            if loaigia_obj:
-                obj.LoaiGia = loaigia_obj
-            else:
-                loaigia_obj = SimTheoGia.objects.filter(title="Giá khác").last()
-                obj.LoaiGia = loaigia_obj
-        elif gia > 3000000 & gia < 5000000:
-            loaigia_obj = SimTheoGia.objects.filter(title="Sim giá 3 triệu - 5 triệu").last()
-            if loaigia_obj:
-                obj.LoaiGia = loaigia_obj
-            else:
-                loaigia_obj = SimTheoGia.objects.filter(title="Giá khác").last()
-                obj.LoaiGia = loaigia_obj
-        elif gia > 5000000 & gia < 10000000:
-            loaigia_obj = SimTheoGia.objects.filter(title="Sim giá 5 triệu - 10 triệu").last()
-            if loaigia_obj:
-                obj.LoaiGia = loaigia_obj
-            else:
-                loaigia_obj = SimTheoGia.objects.filter(title="Giá khác").last()
-                obj.LoaiGia = loaigia_obj
-        elif gia > 10000000 & gia < 50000000:
-            loaigia_obj = SimTheoGia.objects.filter(title="Sim giá 10 triệu - 50 triệu").last()
-            if loaigia_obj:
-                obj.LoaiGia = loaigia_obj
-            else:
-                loaigia_obj = SimTheoGia.objects.filter(title="Giá khác").last()
-                obj.LoaiGia = loaigia_obj
+            loaigia_obj, created = SimTheoGia.objects.get_or_create(title="Sim dưới 500 nghìn")
+            obj.LoaiGia = loaigia_obj
+        elif gia >= 500000 and gia < 1000000:
+            loaigia_obj, created = SimTheoGia.objects.get_or_create(title="Sim giá 500 nghìn - 1 triệu")
+            obj.LoaiGia = loaigia_obj
+        elif gia >= 1000000 and gia < 3000000:
+            loaigia_obj, created = SimTheoGia.objects.get_or_create(title="Sim giá 1 triệu - 3 triệu")
+            obj.LoaiGia = loaigia_obj
+        elif gia >= 3000000 and gia < 5000000:
+            loaigia_obj, created = SimTheoGia.objects.get_or_create(title="Sim giá 3 triệu - 5 triệu")
+            obj.LoaiGia = loaigia_obj
+        elif gia >= 5000000 and gia < 10000000:
+            loaigia_obj, created = SimTheoGia.objects.get_or_create(title="Sim giá 5 triệu - 10 triệu")
+            obj.LoaiGia = loaigia_obj
+        elif gia >= 10000000 and gia < 50000000:
+            loaigia_obj, created = SimTheoGia.objects.filter(title="Sim giá 10 triệu - 50 triệu")
+            obj.LoaiGia = loaigia_obj
+        elif gia >= 50000000:
+            loaigia_obj, created = SimTheoGia.objects.filter(title="Sim giá trên 50 triệu")
+            obj.LoaiGia = loaigia_obj
         obj.save()
+
 admin.site.register(SanPham, Sim)
 
 
